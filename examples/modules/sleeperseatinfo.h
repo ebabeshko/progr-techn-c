d7588a4ce7ce5ca00e061b6a005a37bf47950c30
Eugene Babeshko <63898296+ebabeshko@users.noreply.github.com>
2024-02-23 21:54:59 +0200

    Initial Commit

---
+++ examples/modules/sleeperseatinfo.h
@@ -0,0 +1,12 @@
+#ifndef SLEEPERSEATINFO_H
+#define SLEEPERSEATINFO_H
+
+#define PUBLIC
+#define PRIVATE static
+#define ESEATRANGE -1
+
+PUBLIC  int is_upper (int n);
+PUBLIC  int is_coupe(int n);
+PUBLIC  int get_coupe_number(int n);
+
+#endif
