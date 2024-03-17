8e87369991ba8d201554026f90603833506b78f4
Eugene Babeshko <63898296+ebabeshko@users.noreply.github.com>
2024-03-17 08:38:11 +0200

    Add hangman.c

---
+++ examples/games/hangman.c
@@ -0,0 +1,291 @@
+/**
+ * @file
+ * @brief Реалізація гри Hangman на мові C
+ * @details
+ * Простий приклад реалізації гри Hangman на мові C
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <ctype.h>  /// для main() - tolower()
+#include <stdio.h>  /// для main(), new_word(), new_guess(), won() - операцій введення / вивдення
+#include <stdlib.h> /// для усіх функцій - exit(), rand() та функцій для роботи з файлами
+#include <string.h> /// для main() - для функцій роботи з рядками strlen, strchr, strcpy
+#include <time.h>   /// для new_game() - використовується разом з srand() для створення нової гри
+
+/*
+ * @brief структура game_instance, що зберігає поточний стан гри
+ */
+struct game_instance {
+
+    char current_word[30];  ///< слово, яке має відгадати користувач
+    char hidden[30];        ///< прихована версія слова, що відображається користувачу
+    int size;               ///< довжина слова
+    int incorrect;          ///< кількість некоректних спроб
+    char guesses[25];       ///< попередні спроби
+    int guesses_size;       ///< розмір масиву спроб
+
+};
+
+// прототипи функцій
+struct game_instance new_game(void);                        // створює нову гру
+int new_guess(char, const char guesses[], int size);        // перевіряє, чи користувач вже вводив цю літеру
+int in_word(char, const char word[], unsigned int size);    // перевіряє, чи є літера у слові
+void picture(int score);                                    // виводить зображення качки
+void won(const char word[], int score);                     // перевіряє, чи користувач виграв або програв
+
+/**
+ * @brief Функція Main
+ * @returns 0 при завершенні
+ */
+int main() {
+
+    struct game_instance game = new_game(); // нова гра створена
+    char guess;                             // літера, яку вводить користувач
+
+    // основний цикл - користувач вводить літери
+    while ((strchr(game.hidden, '_') != NULL) && game.incorrect <= 12) {
+        do {
+            printf("\n****************************\n");
+            printf("Your word: ");
+
+            for (int i = 0; i < game.size; i++) {
+                printf("%c ", game.hidden[i]);
+            }
+
+            if (game.guesses_size > 0) {
+                printf("\nSo far, you have guessed: ");
+                for (int i = 0; i < game.guesses_size; i++) {
+                    printf("%c ", game.guesses[i]);
+                }
+            }
+
+            printf("\nYou have %d guesses left.", (12 - game.incorrect));
+            printf("\nPlease enter a letter: ");
+            scanf(" %c", &guess); // зверніть увагу на пробіл перед %c
+            guess = tolower(guess);
+
+        } while (new_guess(guess, game.guesses, game.guesses_size) != -1);
+
+        game.guesses[game.guesses_size] = guess;    // додає нову літеру до масиву спроб
+        game.guesses_size++;                        // оновлює розмір масиву спроб
+
+        if (in_word(guess, game.current_word, game.size) == 1) {
+            printf("That letter is in the word!");
+            for (int i = 0; i < game.size; i++) {
+                if ((game.current_word[i]) == guess) {
+                    game.hidden[i] = guess;
+                }
+            }
+        }
+        else {
+            printf("That letter is not in the word.\n");
+            (game.incorrect)++;
+        }
+        picture(game.incorrect);
+    }
+
+    won(game.current_word, game.incorrect);
+    return 0;
+}
+
+/**
+ * @brief перевіряє, чи користувач вже вводив літеру
+ * @param new_guess літера, введена користувачем
+ * @param guesses масив попередніх спроб користувача
+ * @param size розмір масиву guesses[]
+ * @returns 1 якщо літера вже вводилася раніше
+ * @returns -1 якщо літера не вводилася
+ */
+int new_guess(char new_guess, const char guesses[], int size) {
+
+    for (int j = 0; j < size; j++) {
+        if (guesses[j] == new_guess) {
+            printf("\nYou have already guessed that letter.");
+            return 1;
+        }
+    }
+
+    return -1;
+}
+
+/**
+ * @brief перевіряє, чи є літера у слові
+ * @param letter літера, введена користувачем
+ * @param word поточне слово
+ * @param size довжина слова
+ * @returns 1 якщо літера є у слові
+ * @returns -1 якщо літери немає у слові
+ */
+int in_word(char letter, const char word[], unsigned int size) {
+
+    for (int i = 0; i < size; i++) {
+        if ((word[i]) == letter) {
+            return 1;
+        }
+    }
+
+    return -1;
+}
+
+/**
+ * @brief створює нову гру - генерує випадкове слово та зберігає у глобальній змінній current_word
+ * @returns current_game - екземпляр нової гри, що містить випадково обране слово, його довжину та приховану версію слова
+ */
+struct game_instance new_game() {
+
+    char word[30]; 
+
+    FILE* fptr;
+    fptr = fopen("./words.txt", "r");
+
+    if (fptr == NULL) {
+        fprintf(stderr, "File not found.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // обчислює кількість слів у файлі за умови що кожне слово записане з нового рядка
+    int line_number = 0;
+    while (fgets(word, 30, fptr) != NULL) {
+        line_number++;
+    }
+
+    rewind(fptr);
+
+    // генерує випадкове число
+    int random_num;
+    srand(time(NULL));
+    random_num = rand() % line_number;
+
+    // обирає випадкове слово
+    int s = 0;
+    while (s <= random_num) {
+        fgets(word, 30, fptr);
+        s++;
+    }
+
+    // форматує рядок
+    if (strchr(word, '\n') != NULL) {
+        word[strlen(word) - 1] = '\0';
+    }
+
+    fclose(fptr);
+
+    // створює екземпляр гри
+    struct game_instance current_game;
+    strcpy(current_game.current_word, word);
+    current_game.size = strlen(word);
+    for (int i = 0; i < (strlen(word)); i++) {
+        current_game.hidden[i] = '_';
+    }
+    current_game.incorrect = 0;
+    current_game.guesses_size = 0;
+
+    return current_game;
+}
+
+/**
+ * @brief перевіряє, чи користувач виграв або програв
+ * @param word слово, яке намагається відгадати користувач
+ * @param score скільки некоректних спроб було створено користувачем
+ * @returns void
+ */
+void won(const char word[], int score) {
+    if (score > 12) {
+        printf("\nYou lost! The word was: %s.\n", word);
+    }
+    else {
+        printf("\nYou won! You had %d guesses left.\n", (12 - score));
+    }
+}
+
+/*
+ * @brief поступово малює качку при некоректних спробах користувача
+ * @param score скільки некоректних спроб було створено користувачем
+ * @returns void
+ */
+void picture(int score) {
+
+    switch (score) {
+
+    case 12:
+        printf("\n      _\n"
+            "  __( ' )> \n"
+            " \\_ < _ ) ");
+        break;
+
+    case 11:
+        printf("\n      _\n"
+            "  __( ' )\n"
+            " \\_ < _ ) ");
+        break;
+
+    case 10:
+        printf("\n      _\n"
+            "  __(   )\n"
+            " \\_ < _ ) ");
+        break;
+
+    case 9:
+        printf("\n        \n"
+            "  __(   )\n"
+            " \\_ < _ ) ");
+        break;
+
+    case 8:
+        printf("\n        \n"
+            "  __(    \n"
+            " \\_ < _ ) ");
+        break;
+
+    case 7:
+        printf("\n        \n"
+            "  __     \n"
+            " \\_ < _ ) ");
+        break;
+
+    case 6:
+        printf("\n        \n"
+            "  _      \n"
+            " \\_ < _ ) ");
+        break;
+
+    case 5:
+        printf("\n        \n"
+            "  _      \n"
+            "   _ < _ ) ");
+        break;
+
+    case 4:
+        printf("\n        \n"
+            "         \n"
+            "   _ < _ ) ");
+        break;
+
+    case 3:
+        printf("\n        \n"
+            "         \n"
+            "     < _ ) ");
+        break;
+
+    case 2:
+        printf("\n        \n"
+            "         \n"
+            "       _ ) ");
+        break;
+
+    case 1:
+        printf("\n        \n"
+            "         \n"
+            "         ) ");
+        break;
+
+    case 0:
+        break;
+
+    default:
+        printf("\n      _\n"
+            "  __( ' )> QUACK!\n"
+            " \\_ < _ ) ");
+        break;
+    }
+}
