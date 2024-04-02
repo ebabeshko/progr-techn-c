beac1a5419453a543c2cfe29bbc24893e3c57fa8
Eugene Babeshko <63898296+ebabeshko@users.noreply.github.com>
2024-04-02 17:56:26 +0300

    Add print_ascii.c

---
+++ examples/strings/print_ascii.c
@@ -0,0 +1,34 @@
+/**
+ * @file print_ascii.c
+ * @brief Виведення таблиці ASCII
+ * @details
+ * Простий приклад виведення ASCII-символів з кодами 0x20..0xFF
+*/
+
+#include <stdio.h>
+
+int main() {
+    int j;
+    char c;
+
+    printf("   |");                 // Перший рядок заголовка
+    for (c = '0'; c <= '9'; c++)
+        printf(" .%c", c);
+    for (c = 'A'; c <= 'F'; c++)
+        printf(" .%c", c);
+    printf("\n");
+
+    printf("   |");                 // Другий рядок заголовка
+    for (int i = 1; i <= 16; i++)
+        printf("---");
+    printf("\n");
+
+    for (int i = 2; i <= 7; i++) {  // Таблиця, рядок за рядком
+        printf(" %d.|", i);
+        for (j = 0; j < 16; j++)    // Виведення символа
+            printf("  %c", i * 16 + j);
+        printf("\n");
+    }
+
+    return 0;
+}
